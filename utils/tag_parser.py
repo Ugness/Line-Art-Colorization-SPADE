2bf6773469cc25616948489beebaed8e6ba6e491
Ugness <wogns98@kaist.ac.kr>
2019-10-16 04:19:05 +0900

    add argument parse

--- utils/tag_parser.py
+++ utils/tag_parser.py
@@ -1,13 +1,20 @@
 import json
 import os
 import pandas as pd
-
+import argparse
 
 if __name__ == '__main__':
-    data = pd.read_csv(os.path.join('safebooru', 'all_data.csv'))
-    tag_list = ['solo', 'white_background']
-    ignore = ['pokemon', 'chibi']
+    parser = argparse.ArgumentParser(description='Download images from given metadata.')
+    parser.add_argument('--savedir', type=str, help='directory to save images')
+    parser.add_argument('--metadata', type=str, metavar='*.csv', help='csv file which contains metadata(urls)')
+    parser.add_argument('--tag_list', nargs='+', help='Tags that you want to parse')
+    parser.add_argument('--ignore', nargs='*', default=[], help='Tags that you do not want to parse')
+    args = parser.parse_args()
+
+    tag_list = args.tag_list
+    ignore = ['pokemon', 'chibi', 'monochrome'] + args.ignore
     checks = len(tag_list)
+    data = pd.read_csv(args.metadata)
 
     parsed_ids = []
     for i in range(len(data['id'])):
@@ -25,4 +32,4 @@ if __name__ == '__main__':
                 break
 
     parsed_data = data.iloc[parsed_ids]
-    parsed_data.to_csv(os.path.join('safebooru', 'parsed_data_{}.csv'.format('_'.join(tag_list))))
+    parsed_data.to_csv(os.path.join(args.savedir, 'parsed_data_{}.csv'.format('_'.join(tag_list))), index=False)
