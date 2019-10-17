a40e0b60967fdf0d081aa4c3bba6b043f08bf5e7
Ugness <wogns98@kaist.ac.kr>
2019-10-17 12:07:26 +0900

    implemented multithread fast download

--- utils/image_downloader.py
+++ utils/image_downloader.py
@@ -7,19 +7,27 @@ import argparse
 import requests
 from tqdm import tqdm
 
-lock = threading.Semaphore(8)
+lock = threading.Semaphore(200)
+
 
 def download(url, file):
-    try:
-        response = requests.get('http:' + data['sample_url'][i], stream=True, timeout=60)
-    except requests.ReadTimeout:
-        lock.release()
-        return
-    if response.status_code == 200:
-        with open(os.path.join(savedir, '{}.png'.format(data['id'][i])), 'wb') as f:
-            f.write(response.content)
-    lock.release()
-    return
+    tries = 0
+    while tries < 5:
+        try:
+            response = requests.get(url, stream=True, timeout=3)
+            tries += 1
+            if response.status_code == 200:
+                with open(file, 'wb') as f:
+                    f.write(response.content)
+            lock.release()
+            return
+        except:
+            if tries >= 5:
+                print("Failed to Download Image {}".format(file), flush=True)
+                lock.release()
+                return
+            # print("Retry to Download Image {}".format(file), flush=True)
+            continue
 
 
 if __name__ == '__main__':
@@ -28,7 +36,9 @@ if __name__ == '__main__':
     parser.add_argument('--metadata', type=str, metavar='*.csv', help='csv file which contains metadata(urls)')
     args = parser.parse_args()
 
+    print("Loading csv")
     data = pd.read_csv(args.metadata)
+    print("Done", flush=True)
     savedir = os.path.join(args.savedir, 'color')
     os.makedirs(savedir, exist_ok=True)
 
@@ -36,32 +46,14 @@ if __name__ == '__main__':
     pbar = tqdm(total=len(data['sample_url']))
     threads = []
     for i in range(len(data['sample_url'])):
-        url = 'http:' + data['sample_url'][i]
+        url = 'http:' + data['sample_url'][i].lstrip('http:')
         file = os.path.join(savedir, '{}.png'.format(data['id'][i]))
         pbar.update(1)
         thread = threading.Thread(target=download, kwargs={'url': url, 'file': file})
         threads.append(thread)
         thread.start()
-
         lock.acquire()
+        # sleep(0.5)
 
     for t in threads:
         t.join()
-    """
-    while True:
-        threads = []
-        for j in range(64):
-            if i > len(data['sample_url']):
-                break
-            url = 'http:' + data['sample_url'][i]
-            file = os.path.join(savedir, '{}.png'.format(data['id'][i]))
-            i += 1
-            pbar.update(1)
-            thread = threading.Thread(target=download, kwargs={'url': url, 'file': file})
-            threads.append(thread)
-            thread.start()
-        for t in threads:
-            t.join()
-        if i > len(data['sample_url']):
-            break
-    """
