c85d26a6c2b6ada263b594ebde6f28ad5c47832d
Taeckyung <terry00123@kaist.ac.kr>
2019-11-21 00:54:27 +0900

    Sketch/hint canvas added.

--- demo/flask_test.py
+++ demo/flask_test.py
@@ -1,10 +1,12 @@
-from flask import Flask, flash, redirect, render_template, request, session, abort
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     return render_template('test.html')
 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=1234)
+    app.run(host='0.0.0.0', port=4321)
--- demo/static/js/draw.js
+++ demo/static/js/draw.js
@@ -1,5 +1,6 @@
 
-var canvas, ctx,
+let canvas, ctx,
+    isCanvasSketch = true,
     brush = {
         x: 0,
         y: 0,
@@ -8,10 +9,11 @@ var canvas, ctx,
         down: false,
     },
     strokes = [],
+    strokesFromOther = [],
     currentStroke = null;
 
 function redraw () {
-    ctx.clearRect(0, 0, canvas.width(), canvas.height());
+    ctx.clearRect(0, 0, canvas[0].width, canvas[0].height);
     ctx.lineCap = 'round';
     for (var i = 0; i < strokes.length; i++) {
         var s = strokes[i];
@@ -28,16 +30,25 @@ function redraw () {
 }
 
 function init () {
-    canvas = $('#draw');
-    canvas.attr({
-        width: window.innerWidth,
-        height: window.innerHeight,
+    $('#hint').attr({
+        width: innerWidth < innerHeight ? innerWidth : innerHeight,
+        height: innerWidth < innerHeight ? innerWidth : innerHeight,
     });
+
+    canvas = $('#sketch');
+    canvas.attr({
+        width: innerWidth < innerHeight ? innerWidth : innerHeight,
+        height: innerWidth < innerHeight ? innerWidth : innerHeight,
+    });
+
     ctx = canvas[0].getContext('2d');
 
     function mouseEvent (e) {
-        brush.x = e.pageX;
-        brush.y = e.pageY;
+        let rect = canvas[0].getBoundingClientRect(),
+            scaleX = canvas[0].width / rect.width,
+            scaleY = canvas[0].height / rect.height;
+        brush.x = (e.pageX - rect.left) * scaleX;
+        brush.y = (e.pageY - rect.top) * scaleY;
 
         currentStroke.points.push({
             x: brush.x,
@@ -46,29 +57,30 @@ function init () {
 
         redraw();
     }
-
-    canvas.mousedown(function (e) {
+    function mouseDown (e) {
         brush.down = true;
-
         currentStroke = {
             color: brush.color,
             size: brush.size,
             points: [],
         };
-
         strokes.push(currentStroke);
-
         mouseEvent(e);
-    }).mouseup(function (e) {
+    }
+    function mouseUp (e) {
         brush.down = false;
-
         mouseEvent(e);
-
         currentStroke = null;
-    }).mousemove(function (e) {
+    }
+    function mouseMove (e) {
         if (brush.down)
             mouseEvent(e);
-    });
+    }
+
+    canvas.mousedown(mouseDown)
+        .mouseup(mouseUp)
+        .mousemove(mouseMove);
+
 
     $('#save-btn').click(function () {
         window.open(canvas[0].toDataURL());
@@ -84,6 +96,54 @@ function init () {
         redraw();
     });
 
+    function switchCanvas(isTargetSketch) {
+        if (isTargetSketch === isCanvasSketch) {
+            return
+        }
+
+        canvas.css('zIndex', '2');
+
+        let nextCanvas;
+        if (isCanvasSketch) {
+            nextCanvas = $('#hint');
+        }
+        else {
+            nextCanvas = $('#sketch');
+        }
+
+        canvas = nextCanvas;
+        isCanvasSketch = !isCanvasSketch;
+
+        canvas.css('zIndex', '3');
+        ctx = canvas[0].getContext('2d');
+
+        let temp = strokes;
+        strokes = strokesFromOther;
+        strokesFromOther = temp;
+
+        canvas.mousedown(mouseDown)
+            .mouseup(mouseUp)
+            .mousemove(mouseMove);
+
+        redraw()
+    }
+
+    $('#toSketch').click(function () {
+        $('#toHint').removeClass('selected');
+        $('#toSketch').addClass('selected');
+
+        switchCanvas(true)
+    });
+
+    $('#toSketch').click();
+
+    $('#toHint').click(function () {
+        $('#toSketch').removeClass('selected');
+        $('#toHint').addClass('selected');
+
+        switchCanvas(false)
+    });
+
     $('#color-picker').on('input', function () {
         brush.color = this.value;
     });
@@ -91,6 +151,7 @@ function init () {
     $('#brush-size').on('input', function () {
         brush.size = this.value;
     });
+
 }
 
 $(init);
