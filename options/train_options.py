9e006670012ce54523dc2877e29d1786ca6d24ed
Ugness <wogns98@kaist.ac.kr>
2019-11-26 20:34:11 +0900

    default option change

--- options/train_options.py
+++ options/train_options.py
@@ -34,7 +34,7 @@ class TrainOptions(BaseOptions):
         if opt.no_TTUR:
             parser.set_defaults(beta1=0.5, beta2=0.999)
 
-        parser.add_argument('--lr', type=float, default=0.001, help='initial learning rate for adam')
+        parser.add_argument('--lr', type=float, default=0.0002, help='initial learning rate for adam')
         parser.add_argument('--D_steps_per_G', type=int, default=1, help='number of discriminator iterations per generator iterations.')
 
         # for discriminators
